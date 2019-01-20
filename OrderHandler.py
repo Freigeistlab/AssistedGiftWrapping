a3f37c0c6b65f81c1b183848ecb6e9bf432ec24e
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2019-01-20 18:30:19 +0100

    adds order status update whenever we pull a new order

--- OrderHandler.py
+++ OrderHandler.py
@@ -14,9 +14,14 @@ class OrderHandler:
         self.cur = self.conn.cursor()
 
     def get_open_orders(self):
-        self.cur.execute("SELECT * FROM orders ORDER BY created_at ASC")
+        self.cur.execute("SELECT * FROM orders WHERE order_status_id = 2 ORDER BY created_at ASC")
         for order in self.cur.fetchall():
             self.orders.put_nowait(order)
+
+        self.cur.execute("SELECT * FROM orders WHERE order_status_id = 1 ORDER BY created_at ASC")
+        for order in self.cur.fetchall():
+            self.orders.put_nowait(order)
+
         if not self.orders.empty():
             self.get_next_order()
 
@@ -26,5 +31,11 @@ class OrderHandler:
             self.get_next_order()
 
     def get_next_order(self):
+        current_order_id = str(self.current_order[0])
+        if self.current_order is not None:
+            self.cur.execute("UPDATE orders SET order_status_id = 3 WHERE id = %s ;", str(current_order_id))
         self.current_order = self.orders.get_nowait()
+        current_order_id = str(self.current_order[0])
+        self.cur.execute("UPDATE orders SET order_status_id = 2 WHERE id = %s ;", str(current_order_id))
+        self.conn.commit()
         self.on_new_order()
